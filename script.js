12ef19a68c5baa0ae31e38c80ede917e5c89996f
johnninamanato <144648711+johnninamanato@users.noreply.github.com>
2023-10-23 11:46:54 +0800

    made little changes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
+
   const coffeeMenu = document.querySelector(".menu-links-coffee");
   if (menu) {
     menu.classList.toggle("open");
@@ -10,6 +11,7 @@ function toggleMenu() {
     coffeeMenu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
   const frappeMenu = document.querySelector(".menu-links-frappe");
   if (menu) {
     menu.classList.toggle("open");
