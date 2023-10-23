76738c229d095ae5934fe836c847dce6a6f7a137
Ange-Blry <angelyn.beloria@tup.edu.ph>
2023-10-23 18:50:25 +0800

    addition

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
+  const icontea = document.querySelector(".hamburger-icon-tea");
   const coffeeMenu = document.querySelector(".menu-links-coffee");
   const frappeMenu = document.querySelector(".menu-links-frappe");
+  const teaMenu = document.querySelector(".menu-links-tea");
   if (menu) {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
@@ -14,6 +16,9 @@ function toggleMenu() {
   } else if (coffeeMenu) {
     coffeeMenu.classList.toggle("open");
     icon.classList.toggle("open");
+  } else if (teaMenu) {
+    teaMenu.classList.toggle("open");
+    icontea.classList.toggle("open");
   }
 }
 
@@ -52,40 +57,3 @@ for (var i = 0; i < textOverImages.length; i++) {
   };
 }
 
-/* Sa drinks di paayos
-let items = document.querySelectorAll('.slider .item');
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-
-let active = 3;
-function loadShow() {
-  let stt = 0;
-  items[active].style.transform = 'none';
-  items[active].style.zIndex = 1;
-  items[active].style.filter = 'none';
-  items[active].style.opacity = 1;
-  for (var i = active + 1; i < items.length; i++){
-    stt++;
-    items[i].style.transform = 'translateX(${120*stt}px) scale (${1-0.2stt}) perspective(16px) rotateY(-1deg';
-    items[i].style.zIndex = -stt;
-    items[i].style.filter = 'blur(5px';
-    items[i].style.opacity = stt > 2 ? 0 : 0.6;
-  }
-  stt = 0;
-  for (var i = active - 1; 1 >= 0; i--){
-    stt++;
-    items[i].style.transform = 'translateX(${120*stt}px) scale (${1-0.2stt}) perspective(16px) rotateY(-1deg';
-    items[i].style.zIndex = -stt;
-    items[i].style.filter = 'blur(5px';
-    items[i].style.opacity = stt > 2 ? 0 : 0.6;
-  }
-}
-loadShow();
-next.onclick = function () {
-  active = active + 1 < items.length ? active + 1 : active;
-  loadShow();
-}
-prev.onclick - function () {
-  active = active - 1 >= 0 ? active - 1 : active;
-  loadShow();
-}*/
